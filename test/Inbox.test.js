60d1d71961b777d1df457c2cb2d7749beceb489e
Nik <nikhammerellis@gmail.com>
2018-09-15 10:23:11 -0700

    deploy script implemented, it is live on rinkeby, etherscan confirmed

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,11 +3,13 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 //make an instance of Web3 using constructor function, and tells that instance to attempt to connect
 //to this local test network hosted on my machine
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
@@ -19,10 +21,24 @@ beforeEach(async () => {
       arguments: ["Hi there!"]
     })
     .send({ from: accounts[0], gas: "1000000" }); //instructs web3 to send out a transaction that creates this contract
+
+  inbox.setProvider(provider);
 });
 
+//inbox is the contract, .methods is all of the methods on that contract.
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("Bye!").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Bye!");
   });
 });
